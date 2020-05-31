830c2b3f3d75351e4fbf17fb85b425ee9bf5d1c3
guccibase <tahiru@tryhungry.com>
2020-05-31 00:50:47 -0400

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Tahiru nasuru on 5/31/20.
+//  Copyright © 2020 Tahiru nasuru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
